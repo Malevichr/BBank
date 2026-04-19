b57042ffff3cfd9c0bc042472ffb41767a27c9db
Danila <rexerofficial@gmail.com>
2026-04-19 20:02:31 +0300

    fix

--- app/src/main/java/ru/malevichrp/bbank/features/profile/data/AvatarRepository.kt
+++ app/src/main/java/ru/malevichrp/bbank/features/profile/data/AvatarRepository.kt
@@ -1,25 +1,10 @@
 package ru.malevichrp.bbank.features.profile.data
 
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import ru.malevichrp.bbank.core.DomainError
 import ru.malevichrp.bbank.core.LoadResult
 import ru.malevichrp.bbank.features.profile.presentation.ImageSource
 
 interface AvatarRepository {
     fun load(): Flow<LoadResult<ImageSource>>
-    class Fake() : AvatarRepository {
-        private var shouldError = true
-        override fun load(): Flow<LoadResult<ImageSource>> = flow {
-            delay(1000)
-            if (shouldError) {
-                emit(LoadResult.Error(DomainError.))
-                shouldError = false
-            } else {
-                emit(LoadResult.Success(ImageSource.Empty))
-            }
-        }
 
-    }
 }
